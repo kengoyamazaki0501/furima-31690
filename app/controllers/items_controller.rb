313b686b924a89dd8a4a1b7543248884d98cbe63
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-11-15 14:18:17 +0900

    アクティブハッシュ導入〜モデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,27 @@
 class ItemsController < ApplicationController
   def index
     # @items = Item.includes(:user)
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
   end
 
-  # def create
-  #   @item = Item.new(item_params)
-  #   if @item.save
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:product_name,:explanation,:category_id,:condition_id,:burden_id,:country_id,:delivery_days_id,:price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :burden, :country, :delivery_days
 
   validates     :product_name,     presence: true
   validates     :explanation,      presence: true
@@ -8,9 +10,20 @@ class Item < ApplicationRecord
   validates     :country_id,       presence: true
   validates     :delivery_days_id, presence: true
   validates     :price,            presence: true
-  validates     :image.            presence: true
+  validates     :image,            presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates     :category_id
+    validates     :condition_id
+    validates     :burden_id
+    validates     :country_id
+    validates     :delivery_days_id
+  end
+
+
 
   belongs_to :user
   has_one :order
   has_one_attached :image
+
 end
