a99dcdff6c3761cbff4255154946d26bf12257d5
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-11-16 19:32:15 +0900

    itemモデルのテストコード

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -9,3 +9,17 @@ window.addEventListener('load', () => {
   });
 });
 
+// window.addEventListener('load', () => {
+//   const priceInput = document.getElementById("item-price");
+//   const addTaxDom = document.getElementById("add-tax-price");
+//   const profitDom = document.getElementById("profit");
+//   priceInput.addEventListener("input", () => {
+//     const inputValue = priceInput.value;
+//      const taxvalue = Math.floor(inputValue * 0.1) ;
+//     //  const profitvalue = Math.floor(inputValue * 0.9) 
+//      const tax = taxvalue.toLocaleString() ;
+//      console.log(taxvalue)
+//     addTaxDom.innerHTML = tax;
+//     // profitDom.innerHTML
+//   });
+// });
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates     :product_name
     validates     :explanation
-    validates     :price
     validates     :image
+    validates     :price, numericality: {with:(/\A[0-9]+\z/)}
+    # validates     :price, length: { minimum: 300 },length: { maximum: 9999999 }              
   end
 
-  with_options numericality: { other_than: 1 } do
+  validates_inclusion_of :price, in: 300..9999999, message:'is not range'
+
+  with_options presence: true, numericality: { other_than: 1 } do
     validates     :category_id
     validates     :condition_id
     validates     :burden_id
@@ -16,10 +19,15 @@ class Item < ApplicationRecord
     validates     :delivery_days_id
   end
 
+  belongs_to_active_hash :category 
+  belongs_to_active_hash :condition 
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :country
+  belongs_to_active_hash :delivery_day
+  
 
   belongs_to :user
-  has_one :order
+  # has_one :order
   has_one_attached :image
-  belongs_to :category, :condition, :burden, :country, :delivery_days
-
+  
 end
