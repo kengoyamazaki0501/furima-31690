b256461a29cabc9a2b8f056db79e32b9dc2b8811
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-11-17 15:22:51 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
-    @items = Item.all.order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
+    # @item = Item.includes(params[:id])
   end
 
   def new
@@ -17,7 +18,15 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  # private
+
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :condition_id, :burden_id, :country_id, :delivery_day_id, :price).merge(user_id: current_user.id)
