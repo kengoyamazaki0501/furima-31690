3fbd26416c0c13cb4199778010a525d06e916ae6
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-12-01 11:32:19 +0900

    itemテスコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,73 +20,73 @@ describe Item do
       it '商品名が空では出品できない' do
         @item.product_name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Product name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
 
       it '商品の画像が空では出品できないこと' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("画像を入力してください")
       end
 
       it '商品説明が空では出品できないこと' do
         @item.explanation = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
 
       it 'カテゴリーの情報が必須であること' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category must be other than 1')
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
       end
 
       it '商品の状態についての情報が必須であること' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Condition must be other than 1')
+        expect(@item.errors.full_messages).to include('商品の状態を選択してください')
       end
 
       it '配送料の負担についての情報が必須であること' do
         @item.burden_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Burden must be other than 1')
+        expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
       end
 
       it '発送元の地域についての情報に誤りがあるとき' do
         @item.country_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Country must be other than 1')
+        expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
       end
 
       it '発送日までの日数についての情報に誤りがあるとき' do
         @item.delivery_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery days must be other than 1')
+        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
       end
 
       it '価格が入力されてないとき' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("販売価格を入力してください")
       end
 
       it '半角数字意外の文字が価格に入力されたとき' do
         @item.price = '１１１１１１'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not a number')
+        expect(@item.errors.full_messages).to include('販売価格は数値で入力してください')
       end
 
       it '300以下の金額が入力されたとき' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not range')
+        expect(@item.errors.full_messages).to include('販売価格を¥300〜¥9,999,999の間で入力してください')
       end
 
       it '9,999,999以上の金額が入力されたとき' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not range')
+        expect(@item.errors.full_messages).to include('販売価格を¥300〜¥9,999,999の間で入力してください')
       end
     end
   end
